60917af66547ac78dc60e3f6595b69f4d4df7a23
Klamotek <113595872+Klamotek@users.noreply.github.com>
2023-07-19 11:58:38 +0200

    Update colabfold_temp.sh

--- colabfold_temp.sh
+++ colabfold_temp.sh
@@ -6,14 +6,14 @@
 #
 # Purpose           : Submit protein for prediction to ColabFold in Ubelix
 
-# Parameters for gpu -> gres and mem-per-cpu (remember colabfold is able to work only on one gpu!)
+# Parameters for gpu -> gres and mem-per-cpu (remember colabfold is able to work only on one GPU!)
 # rtx3090 (gpu:rtx3090:1) | required mem = 12G - 16G | max length probably 1000-1400 aa
 # tesla p100 (gpu:teslap100:1) | required mem = 16G - 20G | max length probably ~2000 aa
 # Please make sure you use the lowest possible values 
 
 # REMEMBER TO SET TIME (format hh:mm:ss -> 03:00:00 = 3h)
 # Time values depend on the length of the protein
-# <500 -> up to 3h | 500-1500 -> 3-16h | 1500-2000 -> up to 36h probably
+# <500 -> up to 3h | 500-1500 -> 3-16h | 1500-2000 -> up to 36h 
 
 # !!!!!!!!!!!! MAKE SURE YOU FILLED UP ALL <> PLACEHOLDERS !!!!!!!!!!!!
 # (you have to also delete all the placeholders)
@@ -38,8 +38,6 @@ job_name=$(echo $job_name | tr -d '"')
 
 output_dir="<PATH_TO_OUTPUT_DIR>/$job_name"
 mkdir -p $output_dir
-
-output_dir=<PATH_TO_OUTPUT_DIR>
 
 input=<PATH_TO_INPUT_DIR>
 
